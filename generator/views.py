c03dd2bbb1d2a6001b83a0f289774c78e938fa6f
Mohammed Abu-Al Mahadi <47682811+Mahadi001@users.noreply.github.com>
2021-05-17 02:09:59 +0600

    password generator changed

--- generator/views.py
+++ generator/views.py
@@ -7,18 +7,22 @@ def home(request):
 
 def password(request):
     
-    length = int(request.GET.get('length', 12))
-    characters = list('abcdefghijklmnopqrstuvwxyz')
+    length = int(request.GET.get('length', 9))
+
+    characters = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*_()')
     password = ''
 
-    if request.GET.get('uppercase'):
-        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('radio_options') == "uppercase":
+        characters = (list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
         
-    if request.GET.get('numbers'):
-        characters.extend(list('1234567890'))
+    if request.GET.get('radio_options') == "lowercase":
+        characters = (list('abcdefghijklmnopqrstuvwxyz'))
+        
+    if request.GET.get('radio_options') == "numbers":
+        characters = (list('1234567890'))
 
-    if request.GET.get('specialcharacters'):
-        characters.extend(list('!@#$%^&*_()'))
+    if request.GET.get('radio_options') == "special_characters":
+        characters = (list('!@#$%^&*_()'))
 
     for x in range(length):
         password += random.choice(characters)
